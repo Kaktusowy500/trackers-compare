a2afc161d96ac8f07fe6834a4f66e476e829a7fb
Kaktusowy500 <k.lewand500@gmail.com>
2024-09-27 16:08:31 +0200

    Try to use keypoints for vit tracker improvement

--- trackers/TrackerModVIT.cpp
+++ trackers/TrackerModVIT.cpp
@@ -1,4 +1,5 @@
 #include "TrackerModVIT.hpp"
+#include <limits> 
 
 namespace cv {
 
@@ -50,6 +51,9 @@ namespace cv {
 
     protected:
         void preprocess(const Mat& src, Mat& dst, Size size);
+        double compare_with_template(const Mat& img);
+        void update_template(const Mat& img);
+        void fix_rect(Rect& rect, const Mat& image);
 
         const Size searchSize{ 256, 256 };
         const Size templateSize{ 128, 128 };
@@ -58,6 +62,7 @@ namespace cv {
 
         dnn::Net net;
         Mat image;
+        cv::Mat template_descriptors;
     };
 
     static void crop_image(const Mat& src, Mat& dst, Rect box, int factor)
@@ -78,6 +83,59 @@ namespace cv {
         Rect roi(x1 + x1_pad, y1 + y1_pad, x2 - x2_pad - x1 - x1_pad, y2 - y2_pad - y1 - y1_pad);
         Mat im_crop = src(roi);
         copyMakeBorder(im_crop, dst, y1_pad, y2_pad, x1_pad, x2_pad, BORDER_CONSTANT);
+    }
+
+    double TrackerModVITImpl::compare_with_template(const cv::Mat& img)
+    {
+        cv::Ptr<cv::ORB> orb = cv::ORB::create();
+        std::vector<cv::KeyPoint> keypoints;
+        cv::Mat img_decscriptors;
+
+        orb->detectAndCompute(img, cv::Mat(), keypoints, img_decscriptors);
+
+        cv::BFMatcher matcher(cv::NORM_HAMMING);
+        std::vector<cv::DMatch> matches;
+        if (img_decscriptors.empty())
+        {
+            return -1;
+        }
+        matcher.match(template_descriptors, img_decscriptors, matches);
+        // std::cout << "matches size: " << matches.size() << "img desc size: " << img_decscriptors.rows << " template desc size: " << template_descriptors.rows << std::endl;
+        std::sort(matches.begin(), matches.end());
+        // for (auto & m : matches)
+        // {
+        //     std::cout << m.distance  << " ";
+        // }
+        // std::cout << std::endl;
+
+        double sumDistances = 0.0;
+        int count = std::min(10, static_cast<int>(matches.size()));
+        for (int i = 0; i < count; ++i) {
+            sumDistances += matches[i].distance;
+        }
+        double averageDistance = count > 0 ? sumDistances / count : -1;
+        return averageDistance;
+    }
+    void TrackerModVITImpl::fix_rect(Rect& rect, const Mat& image)
+    {
+        rect.x = rect.x < 0 ? 0 : rect.x;
+        rect.y = rect.y < 0 ? 0 : rect.y;
+        rect.width = (rect.width + rect.x) > image.cols ? image.cols - rect.x : rect.width;
+        rect.height = (rect.height + rect.y) > image.rows ? image.rows - rect.y : rect.height;
+    }
+
+
+    void TrackerModVITImpl::update_template(const cv::Mat& img)
+    {
+        std::cout << "update template" << std::endl;
+        cv::Ptr<cv::ORB> orb = cv::ORB::create();
+        std::vector<cv::KeyPoint> keypoints;
+        cv::Mat descriptors;
+        orb->detectAndCompute(img, cv::Mat(), keypoints, descriptors);
+        if (descriptors.rows > 10)
+        {
+            template_descriptors = descriptors;
+        }
     }
 
     void TrackerModVITImpl::preprocess(const Mat& src, Mat& dst, Size size)
@@ -102,6 +160,29 @@ namespace cv {
         return static_cast<double>(intersection_area) / union_area;
     }
 
+    void postprocessDistances(std::vector<double>& distances) {
+        double maxDist = *std::max_element(distances.begin(), distances.end());
+        for (auto& dist : distances)
+        {
+            if (dist < 0)
+                dist = maxDist;
+        }
+    }
+
+    std::vector<double> normalizeDistances(const std::vector<double>& distances) {
+        double maxDist = *std::max_element(distances.begin(), distances.end());
+        double minDist = *std::min_element(distances.begin(), distances.end());
+
+        if (maxDist - minDist < 0.0001)
+            return std::vector<double>(distances.size(), 0.0);
+
+        std::vector<double> normalizedDists(distances.size());
+        for (size_t i = 0; i < distances.size(); ++i) {
+            normalizedDists[i] = (distances[i] - minDist) / (maxDist - minDist);
+        }
+
+        return normalizedDists;
+    }
 
     static Mat hann1d(int sz, bool centered = true) {
         Mat hanningWindow(sz, 1, CV_32FC1);
@@ -161,6 +242,7 @@ namespace cv {
         Size size(16, 16);
         hanningWindow = hann2d(size, false);
         rect_last = boundingBox_;
+        update_template(image(rect_last));
     }
 
     bool TrackerModVITImpl::update(InputArray image_, Rect& boundingBoxRes)
@@ -184,10 +266,13 @@ namespace cv {
 
         std::vector<Rect> maxRects;
         std::vector<double> maxScores;
+        std::vector<double> avgSimilarityDistances;
         Mat conf_map_copy = conf_map.clone();
 
+        static constexpr int numCandidates = 5;
+
         //Take 5 highest scores
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < numCandidates; i++)
         {
             double maxVal;
             Point maxLoc;
@@ -201,16 +286,19 @@ namespace cv {
             float h = size_map.at<float>(1, maxLoc.y, maxLoc.x);
 
             Rect candidate_rect = returnfromcrop(cx - w / 2, cy - h / 2, w, h, rect_last);
+            fix_rect(candidate_rect, image);
             // std::cout << candidate_rect << "score: " << tracking_score << std::endl;
             maxRects.push_back(candidate_rect);
             maxScores.push_back(tracking_score);
+            avgSimilarityDistances.push_back(compare_with_template(image(candidate_rect)));
             conf_map_copy.at<float>(maxLoc.y, maxLoc.x) = 0;
             cv::rectangle(image, candidate_rect, cv::Scalar(255, 0, 0), 2, 1);
         }
-        // cv::imshow("ModVIT", image);
+        cv::imshow("ModVIT", image);
+        postprocessDistances(avgSimilarityDistances);
+        std::vector<double> normalizedDists = normalizeDistances(avgSimilarityDistances);
 
         int highestScoreIndex = 0;
-
         // simillar confs of the best two candidates
         if (maxScores[0] < 1.2 * maxScores[1]) {
             // postprocessed scores
@@ -219,11 +307,14 @@ namespace cv {
             for (int i = 0; i < 3; i++)
             {
                 double candidateScore = 0;
-                for (int j = 0; j < 5; j++)
+                for (int j = 0; j < numCandidates; j++)
                 {
                     candidateScore += calculate_overlap(maxRects[i], maxRects[j]) * maxScores[j];
                 }
                 candidateScore *= maxScores[i];
+                double featureDistanceFactor = normalizedDists[i] * 0.05;
+                std::cout << "candidate score: " << candidateScore << "normalizedDist" << featureDistanceFactor << std::endl;
+                // candidateScore -= featureDistanceFactor;
                 candidatesScores.push_back(candidateScore);
             }
 
@@ -233,9 +324,14 @@ namespace cv {
                 std::cout << "non zero index"; // for debugging
 
         }
+        // std::cout << "Score: " << compare_with_template(image(maxRects[highestScoreIndex])) << std::endl;
+        if (maxScores[highestScoreIndex] > 0.7)
+        {
+            update_template(image(maxRects[highestScoreIndex]));
+        }
 
         rect_last = maxRects[highestScoreIndex];
-        boundingBoxRes = maxRects[highestScoreIndex];
+        boundingBoxRes = rect_last;
         tracking_score = maxScores[highestScoreIndex];
         return true;
     }
